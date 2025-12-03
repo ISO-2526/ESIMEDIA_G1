83b6de274a75808199b60087a9a420dd191a1dc6
Carcapri <carlos.caceres@alu.uclm.es>
2025-12-03 17:54:53 +0100

    añadido filtro a securityconfig

--- esimedia/src/main/java/grupo1/esimedia/Accounts/config/SecurityConfig.java
+++ esimedia/src/main/java/grupo1/esimedia/Accounts/config/SecurityConfig.java
@@ -2,24 +2,35 @@ package grupo1.esimedia.Accounts.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import grupo1.esimedia.security.SessionTimeoutFilter;
+
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    private final SessionTimeoutFilter sessionTimeoutFilter; 
+
+    // Constructor para inyectar el filtro
+    public SecurityConfig(SessionTimeoutFilter sessionTimeoutFilter) {
+        this.sessionTimeoutFilter = sessionTimeoutFilter;
     }
     
     @Bean
@@ -60,7 +71,9 @@ public class SecurityConfig {
                 
                 // Todas las demás peticiones requieren autenticación
                 .anyRequest().authenticated()
-            );
+                
+            )
+            .addFilterBefore(sessionTimeoutFilter, UsernamePasswordAuthenticationFilter.class);
         
         return http.build();
     }
